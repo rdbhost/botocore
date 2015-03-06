107f22bbd328debd25fec7d03780fdff6ccfa1eb
David <dkeeney@travelbyroad.net>
2015-03-05 19:53:57 -0700

    test_service tests pass

--- tests/unit/test_service.py
+++ tests/unit/test_service.py
@@ -12,6 +12,10 @@
 # ANY KIND, either express or implied. See the License for the specific
 # language governing permissions and limitations under the License.
 import mock
+import asyncio
+import sys
+sys.path.append('..')
+from asyncio_test_utils import async_test, future_wrapped
 
 from tests import BaseSessionTest
 
@@ -23,62 +27,70 @@ class TestService(BaseSessionTest):
         super(TestService, self).setUp()
         self.environ['AWS_CONFIG_FILE'] = 'nowhere-foobar'
 
+    @async_test
     def test_get_endpoint_with_no_region(self):
         # Test global endpoint service such as iam.
-        service = self.session.get_service('iam')
+        service = yield from self.session.get_service('iam')
         endpoint = service.get_endpoint()
         self.assertEqual(endpoint.host, 'https://iam.amazonaws.com')
 
+    @async_test
     def test_get_endpoint_forwards_verify_args(self):
-        service = self.session.get_service('iam')
+        service = yield from self.session.get_service('iam')
         endpoint = service.get_endpoint(verify='/path/cacerts.pem')
         self.assertEqual(endpoint.verify, '/path/cacerts.pem')
 
+    @async_test
     def test_endpoint_arg_overrides_everything(self):
-        service = self.session.get_service('iam')
+        service = yield from self.session.get_service('iam')
         endpoint = service.get_endpoint(
             region_name='us-east-1',
             endpoint_url='https://wherever.i.want.com')
         self.assertEqual(endpoint.host, 'https://wherever.i.want.com')
         self.assertEqual(endpoint.region_name, 'us-east-1')
 
+    @async_test
     def test_service_metadata_not_required_for_region(self):
-        service = self.session.get_service('iam')
+        service = yield from self.session.get_service('iam')
         endpoint = service.get_endpoint(
             region_name='us-east-1',
             endpoint_url='https://wherever.i.want.com')
         self.assertEqual(endpoint.host, 'https://wherever.i.want.com')
         self.assertEqual(endpoint.region_name, 'us-east-1')
 
+    @async_test
     def test_region_not_required_if_endpoint_url_given(self):
         # Only services that require the region_name (sigv4)
         # should require this param.  If we're talking to
         # a service that doesn't need this info, there's no
         # reason to require this param in botocore.
-        service = self.session.get_service('importexport')
+        service = yield from self.session.get_service('importexport')
         endpoint = service.get_endpoint(
             endpoint_url='https://wherever.i.want.com')
         self.assertEqual(endpoint.host, 'https://wherever.i.want.com')
         self.assertIsNone(endpoint.region_name)
 
+    @async_test
     def test_region_required_for_non_global_endpoint(self):
         # If you don't provide an endpoint_url, than you need to
         # provide a region_name.
-        service = self.session.get_service('ec2')
+        service = yield from self.session.get_service('ec2')
         with self.assertRaises(botocore.exceptions.UnknownEndpointError):
             service.get_endpoint()
 
+    @async_test
     def test_region_not_required_if_endpoint_url_given(self):
         # Even if we're not a global service, if an endpoint_url is given
         # then we still construct an endpoint even if the heuristics fail.
-        service = self.session.get_service('sdb')
+        service = yield from self.session.get_service('sdb')
         endpoint = service.get_endpoint(
             region_name=None,
             endpoint_url='http://custom-endpoint/')
         self.assertEqual(endpoint.host, 'http://custom-endpoint/')
 
+    @async_test
     def test_turnoff_signing(self):
-        service = self.session.get_service('ec2')
+        service = yield from self.session.get_service('ec2')
         service.signature_version = None
         with mock.patch('botocore.endpoint.EndpointCreator.create_endpoint') \
                 as mock_create_endpoint:
