7b7fe0de03bf7b43e651b7fa1ab6da0b4885abcc
David <dkeeney@travelbyroad.net>
2015-03-06 21:57:58 -0700

    integration tests in test_rds pass,

--- tests/integration/test_rds.py
+++ tests/integration/test_rds.py
@@ -11,30 +11,41 @@
 # ANY KIND, either express or implied. See the License for the specific
 # language governing permissions and limitations under the License.
 
+
 import unittest
 import itertools
+import asyncio
+import sys
+sys.path.append('..')
+from asyncio_test_utils import async_test, future_wrapped, pump_iter
 
 import botocore.session
 
 
 class TestRDSPagination(unittest.TestCase):
-    def setUp(self):
+
+    @asyncio.coroutine
+    def set_up(self):
         self.session = botocore.session.get_session()
-        self.service = self.session.get_service('rds')
+        self.service = yield from self.session.get_service('rds')
         self.endpoint = self.service.get_endpoint('us-west-2')
 
+    @async_test
     def test_can_paginate_reserved_instances(self):
         # Using an operation that we know will paginate.
         operation = self.service.get_operation('DescribeReservedDBInstancesOfferings')
         generator = operation.paginate(self.endpoint)
-        results = list(itertools.islice(generator, 0, 3))
+        genResults = yield from pump_iter(generator)
+        results = list(itertools.islice(genResults, 0, 3))
         self.assertEqual(len(results), 3)
         self.assertTrue(results[0][1]['Marker'] != results[1][1]['Marker'])
 
+    @async_test
     def test_can_paginate_orderable_db(self):
         operation = self.service.get_operation('DescribeOrderableDBInstanceOptions')
         generator = operation.paginate(self.endpoint, engine='mysql')
-        results = list(itertools.islice(generator, 0, 2))
+        genResults = yield from pump_iter(generator)
+        results = list(itertools.islice(genResults, 0, 2))
         self.assertEqual(len(results), 2)
         self.assertTrue(results[0][1].get('Marker') != results[1][1].get('Marker'))
 
